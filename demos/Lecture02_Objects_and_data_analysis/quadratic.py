96466c6a63904cb66f80e4d2ef01c7e192283f15
Henri Drake <henrifdrake@gmail.com>
2020-01-20 10:31:24 -0500

    Reorganized things and updated Lecture02 notebook

--- demos/Lecture02_Objects_and_data_analysis/quadratic.py
+++ demos/Lecture02_Objects_and_data_analysis/quadratic.py
@@ -1,3 +1,9 @@
+
+def quadratic_roots(a, b, c):
+    x1 = (-b + (b**2 - 4. *a*c)**0.5)/(2*a)
+    x2 = (-b - (b**2 - 4. *a*c)**0.5)/(2*a)
+    return x1, x2 # we can return more than one variable
+
 class quadratic():
     def __init__(self, a, b, c):
         self.a = a
@@ -6,6 +12,9 @@ class quadratic():
         
     def evaluate(self, x):
         return self.a * x**2 + self.b * x + self.c
+    
+    def calc_roots(self):
+        return quadratic_roots(self.a, self.b, self.c)
 
 class parabola(quadratic):
     def __init__(self, a, c):
